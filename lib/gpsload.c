e98d558963fdf0b4468a4ee363c4c5c56f188cce
marc <marc>
2001-06-13 22:21:26 +0000

    copyright change -- explicitly allow modification

--- lib/gpsload.c
+++ lib/gpsload.c
@@ -1,14 +1,15 @@
 /*
- *	$snafu: gpsload.c,v 1.3 2001/06/12 17:27:13 marc Exp $
+ *	$snafu: gpsload.c,v 1.4 2001/06/13 22:21:27 marc Exp $
  *
  *	Copyright (c) 1998 Marco S. Hyman
  *
- *	Permission to copy all or part of this material for any purpose is
- *	granted provided that the above copyright notice and this paragraph
- *	are duplicated in all copies.  THIS SOFTWARE IS PROVIDED ``AS IS''
- *	AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, WITHOUT
- *	LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *	FOR A PARTICULAR PURPOSE.
+ *	Permission to copy all or part of this material with or without
+ *	modification for any purpose is granted provided that the above
+ *	copyright notice and this paragraph are duplicated in all copies.
+ *
+ *	THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ *	IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #include <stdio.h>
